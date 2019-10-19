78a9390d642d81dd85485b705d346df2cd19e924
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 04:05:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-    deli.each_with_index(1) do |name, index|
+    katz_deli.each_with_index(1) do |name, index|
       new.push("#{index}. #{name}")
   end
   puts "The line is currently: #{new.join(" ")}"
