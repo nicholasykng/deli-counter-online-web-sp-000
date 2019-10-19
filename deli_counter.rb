11dc40668da35cb2c65eab8bac0be502d630ef8e
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 03:51:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
 def line(katz_deli)
+  new = []
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      new.push("#{index}. #{name}")
   end
+  puts "The line is currently: #{new.join(" ")}
 end
 
 def take_a_number(katz_deli, name)
-  counter = 1
   katz_deli. each do |name|
   puts "Welcome, #{name}. You are number #{counter} in line."
   counter += 1
