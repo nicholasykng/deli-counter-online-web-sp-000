901e9258b2ff5b1afc5ceee585c5d2278ca9f3c5
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 03:52:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,9 +11,4 @@ def line(katz_deli)
 end
 
 
-def take_a_number(katz_deli, name)
-  katz_deli. each do |name|
-  puts "Welcome, #{name}. You are number #{counter} in line."
-  counter += 1
-end
-end
+
