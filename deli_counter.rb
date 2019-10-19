ce57cda16ee3fb4713d1931ad569ca82d569998f
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 03:32:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  counter = 1
+  katz_deli. each do |name|
+  puts "Welcome, #{name}. You are number #{counter} in line."
+  counter += 1
+end
+end
